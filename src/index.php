e87db5abd924a92ab0e476121b45347074fd021d
nnssec <tomohiro.namba@gmail.com>
2020-02-02 15:37:52 +0900

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
+    <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+  </head>
+  <body>
+  </body>
+</html>
