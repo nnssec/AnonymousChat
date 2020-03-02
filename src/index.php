ef91eb316d9ca72ce8fff189f457a733c94dc193
nnssec <tomohiro.namba@gmail.com>
2020-03-03 00:08:36 +0900

    表示内容を変更

--- src/index.php
+++ src/index.php
@@ -9,5 +9,11 @@
     <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
   </head>
   <body>
+    <h1>チャット</h1>
+    <form method="post" action="index.php">
+      名前 <input type="text" name="name"><br>
+      メッセージ <input tyep="text" name="message"><br>
+      <button name="send" type="submit">送信</button><br>
+    </form>
   </body>
 </html>
