178c5175ea36600c7f8d50e5ce1ff0c4dc2e6807
nnssec <tomohiro.namba@gmail.com>
2020-03-23 23:35:06 +0900

    ファビコンを変更

--- src/room/index.php
+++ src/room/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
     <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
-    <link rel="shortcut icon" href="../img/favicon.jpg">
+    <link rel="shortcut icon" href="../img/Anonymous Laughing Man.png">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
   </head>
