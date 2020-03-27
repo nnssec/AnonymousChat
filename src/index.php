e92174b569cf2754a3dd35fd66d9f8ecafa2b6b1
nnssec <tomohiro.namba@gmail.com>
2020-03-27 12:36:29 +0900

    プルダウンメニューを削除

--- src/index.php
+++ src/index.php
@@ -8,13 +8,11 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
     <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
     <link rel="shortcut icon" href="img/Anonymous Laughing Man.png">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
   </head>
   <body>
     <header>
-      <img src="img/menu.svg" class="menu">
       <img src="img/Anonymous Laughing Man.png" width="50px">
       <h1>Anonymous Forum</h1><br>
     </header>
